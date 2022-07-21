c71a1a8067cedd654508bef9b303b3fe3f3c5856
jpgomesbrito <jpedrog278@gmail.com>
2022-07-20 22:50:32 -0300

    remove imports

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../app.controller';
 import { MessagesController } from './messages.controller';
-import { AppService } from '../app.service';
 import { MessagesService } from './messages.service';
 
 @Module({
